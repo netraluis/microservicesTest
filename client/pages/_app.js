ccc0c7d01954a60b287ffef276f93b745455531b
funzza <netraluis@gmail.com>
2021-07-30 17:55:30 +0200

    get initial props manipulation

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
 
 // next default component
 
-const App = ({ Component, pageProps }) => {
-  return <Component { ...pageProps } />
+const AppComponent = ({ Component, pageProps, currentuser }) => {
+  return <>
+    <h1>Header! {currentuser.email}</h1>
+    <Component { ...pageProps } />
+   </>
 };
 
-export default App;
+// in a Page context === { req, res }
+// in _app context === { Component, ctx: {req, res }}
+AppComponent.getInitialProps = async appContext => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+  let pageProps = {};
+  if(appContext.Component.getInitialProps){
+    pageProps = appContext.Component.getInitialProps(appContext.ctx)
+  }
+
+  return {
+    pageProps,
+    ...data
+  };
+};
+
+export default AppComponent;
