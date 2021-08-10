07f8918ec67433614d22450be51dbb1ae3c27a53
funzza <netraluis@gmail.com>
2021-08-10 13:10:22 +0200

    test for creating a new route

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../app';
+import { Ticket } from '../../model/ticket';
 
 it('has a route handler listening to /api/tickets for post request', async () => {
   const response = await request(app)
@@ -39,27 +40,39 @@ it('returns an error if an invalid title is provided', async () => {
     
 })
 
-// it('returns an error if an invalid price is provided', async () => {
-//   await request(app)
-//     .post('/api/tickets')
-//     .set('Cookie', global.signin())
-//     .send({
-//       title: 'asdasdasd',
-//       price: -10
-//     })
-//     .expect(400)
+it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'asdasdasd',
+      price: -10
+    })
+    .expect(400)
 
-//   await request(app)
-//     .post('/api/tickets')
-//     .set('Cookie', global.signin())
-//     .send({
-//       title: 'asdasdasd',
-//       price: -10
-//     })
-//     .expect(400)
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'asdasdasd',
+    })
+    .expect(400)
   
-// })
+})
 
-// it('creates a ticket with valid inputs ', async () => {
-  
-// })
+it('creates a ticket with valid inputs ', async () => {
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'asadf', 
+      price: 20
+    })
+    .expect(201)
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+})
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,26 +1,33 @@
 import express, { Request, Response, NextFunction } from 'express';
 import { body } from 'express-validator';
 import { requireAuth, validateRequest, currentUser } from '@nltickets/common';
+import { Ticket } from '../model/ticket';
 
 const router = express.Router();
 
 router.post('/api/tickets', 
-(req: Request, res:Response, next: NextFunction)=> {
-  console.log('hola midd')
-  return next()
-},
 currentUser,
 requireAuth,
 [
   body('title')
-    .not()
-    .isEmpty()
-    .withMessage('Title is required')
+  .not()
+  .isEmpty()
+  .withMessage('Title is required'),
+  body('price')
+  .isFloat({ gt: 0})
+  .withMessage('Price must be grater than 0')
 ], 
 validateRequest, 
 async (req: Request, res: Response, next: NextFunction)=> {
-  console.log('hola')
-  res.status(200)
+  const { title, price } = req.body;
+  const ticket = Ticket.build({
+    title,
+    price,
+    userId: req.currentUser!.id
+  });
+  await ticket.save();
+
+  res.status(201).send(ticket);
 })
 
 export { router as createTicketRouter }
