51f7e1f0dcc81bfbd1627e1195bc81a4c896b048
funzza <netraluis@gmail.com>
2021-08-16 14:11:44 +0200

    interface with generic to control the subject and the data to make typos

--- nats-test/src/events/base-listener.ts
+++ nats-test/src/events/base-listener.ts
@@ -1,9 +1,17 @@
-import nats, { Message, Stan } from 'node-nats-streaming';
+import { Message, Stan } from 'node-nats-streaming';
 
-export abstract class Listener {
-  abstract subject: string;
+import { Subjects } from './subjects';
+
+interface Event {
+  subject: Subjects;
+  data: any;
+}
+export abstract class Listener<T extends Event> {
+  
+  // T['subject'] enters in the interface **Event** and takes the type subject from T
+  abstract subject: T['subject'];
   abstract queueGroupName: string;
-  abstract onMessage(data: any, msg: Message): void;
+  abstract onMessage(data: T['data'], msg: Message): void;
   private client: Stan;
   // protected a subclass can redifined
   protected ackWait = 5 * 1000;
