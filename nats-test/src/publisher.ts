561315061be376dee70e67ed0af6a70cd6ecf73a
funzza <netraluis@gmail.com>
2021-08-17 15:49:38 +0200

    nats test folder finish

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -8,7 +8,7 @@ const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect',async () => {
   console.log('Publisher connected to NATS');
 
   // const data = JSON.stringify({
@@ -22,7 +22,7 @@ stan.on('connect', () => {
   // });
 
   const publisher = new TicketCreatedPublisher(stan);
-  publisher.publish({
+  await publisher.publish({
     id: '123',
     title: 'concert',
     price: 20
