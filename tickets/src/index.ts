4209a14d84bf81a8a628b2af5bc9f82795393c26
funzza <netraluis@gmail.com>
2021-08-20 21:53:06 +0200

    env variables in ticketing

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -12,6 +12,15 @@ const start = async () => {
   if(!process.env.MONGO_URI){
     throw new Error('MONGO_URI must be defined')
   }
+  if(!process.env.NATS_CLIENT_ID){
+    throw new Error('NATS_CLIENT_ID must be defined')
+  }
+  if(!process.env.NATS_URL){
+    throw new Error('NATS_URL must be defined')
+  }
+  if(!process.env.NATS_CLUSTER_ID){
+    throw new Error('NATS_CLUSTER_ID must be defined')
+  }
 
   try{
     /**
@@ -19,7 +28,7 @@ const start = async () => {
      * -cid (cluster id) which is ticketing
      * defines the cluster
      */
-    await natsWrapper.connect('ticketing', randomBytes(4).toString('hex'), 'http://nats-srv:4222');
+    await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
     natsWrapper.client.on('close', () => {
       console.log('NATS connection closed!');
       process.exit();
